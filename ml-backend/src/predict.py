10203e2630000cc9bda893beedae46d06d04b7f3
jose5556 <josefilmeneses@gmail.com>
2026-05-27 13:15:10 +0100

    dependencies requirements

--- ml-backend/src/predict.py
+++ ml-backend/src/predict.py
@@ -200,3 +200,8 @@ if __name__ == "__main__":
 
     print("\nCONFUSING MULTILEVEL TEST:")
     print(predict_vertical_context(confusing_multilevel_sample))
+
+""" 
+source venv/bin/activate
+uvicorn src.api:app --host 0.0.0.0 --port 8000 --reload 
+"""
