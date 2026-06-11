61c0d759a121835c532d0110a0a1a785a7b91bab
vivaccar <viniciusvaccarih@hotmail.com>
2026-06-11 17:34:53 +0100

    added swift.swift

--- app/IOS/swift/Sources/swift/swift.swift
+++ app/IOS/swift/Sources/swift/swift.swift
@@ -4,6 +4,23 @@
 @main
 struct swift {
     static func main() {
-        print("Hello, world!")
+
+        let gps = GpsReading(
+            latitude: 41.14961,
+            longitude: -8.61099,
+            accuracyMeters: 5,
+            altitudeMeters: 120,
+            speedMps: 0,
+            hasSignal: true
+        )
+
+        let motion = MotionSample(ax: 0.1, ay: 0.2, az: 9.8)
+
+        let window = SensorWindow(
+            gpsReadings: [gps],
+            motionSamples: [motion]
+        )
+
+        print(window.gpsReadings.first?.latitude ?? 0)
     }
 }
